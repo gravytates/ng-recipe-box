d85edbadad4491dfffe0235f9949abc36b8a1c7a
Grady Shelton and Tanner Eustice <tanner@>
2017-06-06 11:06:50 -0700

    add data binding to recipes

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,15 +6,32 @@ import { Component } from '@angular/core';
     <h1>Recipes for days</h1>
 
     <ul>
-      <li *ngFor="let recipe of recipes">{{recipe.title}}
-        <ul>
-        <li>{{recipe.description}}</li>
-          <ul>
-          <li *ngFor="let ingredients of recipe.ingredients">{{ingredients}}</li>
-          </ul>
-        </ul>
+      <li  *ngFor="let recipe of recipes" >{{recipe.title}}
+      <button (click)="viewChicken(recipe)">details</button>
       </li>
     </ul>
+
+    <div *ngIf="selectedChicken">
+      <ul>
+      <li>{{selectedChicken.description}}</li>
+        <ul>
+        <li *ngFor="let ingredients of selectedChicken.ingredients">{{ingredients}}</li>
+        </ul>
+      </ul>
+      <hr>
+      <label>Recipe Name:</label>
+      <input [(ngModel)]="selectedChicken.title">
+      <label>Enter Recipe Description:</label>
+      <input [(ngModel)]="selectedChicken.description">
+      <label>Add An Ingredient:</label>
+      <button (click)="hideChicken()">Update</button>
+
+      <br>
+      <br>
+      <input [(ngModel)]="newIngredient">
+      <button (click)="addIngred()">Add ingredient</button>
+      <button (click)="delIngred(selectedChicken)">Delete all Ingredients</button>
+    </div>
   `
 })
 
@@ -31,6 +48,27 @@ export class AppComponent {
     new Recipe('Fried Chicken', 'delicious succulent tasty', ['chicken', 'oil', 'flour', 'egg', 'seasoning']),
     new Recipe('Fried Chicken', 'delicious succulent tasty', ['chicken', 'oil', 'flour', 'egg', 'seasoning'])
   ];
+
+  selectedChicken = null;
+  newIngredient = null;
+  viewChicken(chosenOne) {
+    this.newIngredient = null;
+    this.selectedChicken = chosenOne;
+  }
+
+  addIngred() {
+    if(this.newIngredient != null) {
+      this.selectedChicken.ingredients.push(this.newIngredient);
+    }
+  }
+
+  delIngred(secrets) {
+    secrets.ingredients = [];
+  }
+
+  hideChicken() {
+    this.selectedChicken = null;
+  }
 }
 
 export class Recipe {
